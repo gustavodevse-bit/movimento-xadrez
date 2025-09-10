0f0bea8463e9c423a04b659e5a7020605350dfb1
gustavodevse-bit <gustavodevse@gmail.com>
2025-09-10 15:06:03 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -6,6 +6,7 @@ int main(){
     int bispo = 1;
     int rainha = 1;
     int Torre = 1;
+    int cavalo = 1;
 
     // Torre: movimento em linha reta
     // Simular 5 casas para a direita
@@ -39,9 +40,25 @@ printf("Movimento da Rainha:\n");
     {
        printf("Esquerda\n");
     }
-    
+
+    // Cavalo: movimento em L
+    // Simular 2 casas para baixo e um para Esquerda
+    // Estrutura: for e while
+
+printf("\nMovimento do Cavalo:\n");
+
+while (cavalo > 0) {
+    // Duas casas para baixo
+    for (int i = 0; i < 2; i++) {
+        printf("Baixo\n");
+    }
+    // Uma casa para a esquerda
+    printf("Esquerda\n");
+
+    cavalo--; // reduz contador de movimentos do Cavalo
     
 
 
     return 0;
+}
 }
